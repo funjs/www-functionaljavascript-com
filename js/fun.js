8098b58a1fcdab4353fed95b0ea15004da2deeed
fogus <mefogus@gmail.com>
2013-03-27 10:25:46 -0400

    Stub for glossary lookup

--- js/fun.js
+++ js/fun.js
@@ -1,9 +1,11 @@
 function lookup(term) {
-  console.log("looking up term: " + term);
-  alert('glossary lookup not yet available');
+  alert(['glossary entry for', term, 'not yet available'].join(' '));
 }
 
 $(document).ready(function() {
-  $('.glossary-link').click(lookup);
+  $('.glossary-link').each(function() {
+    var link = $(this);
+    link.click(L(lookup, link.text()));
+  });
 });
 
