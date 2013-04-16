d66f29206b11b8506a56d577d221227cd498f34f
fogus <mefogus@gmail.com>
2013-04-16 09:44:30 -0400

    Added partial

--- dictionary.js
+++ dictionary.js
@@ -1,5 +1,7 @@
 var exports = exports || {};
 
 exports.dictionary = {
-  currying: "The technique of transforming a function that takes multiple arguments so that it can be called as a chain of functions, each with a single argument."
+  currying: "The technique of transforming a function that takes multiple arguments so that it can be called as a chain of functions, each with a single argument.",
+
+  parital: "A function that is 'partially' executed and is ready for immediate execution given the remainder of its expected arguments"
 };
