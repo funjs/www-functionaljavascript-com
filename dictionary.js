02345435b531428a040b3f4e590847ef4045b9d3
fogus <mefogus@gmail.com>
2013-04-16 09:54:31 -0400

    Remainder of definitions, for now

--- dictionary.js
+++ dictionary.js
@@ -3,5 +3,11 @@ var exports = exports || {};
 exports.dictionary = {
   currying: "The technique of transforming a function that takes multiple arguments so that it can be called as a chain of functions, each with a single argument.",
 
-  parital: "A function that is 'partially' executed and is ready for immediate execution given the remainder of its expected arguments"
+  partial: "A function that is 'partially' executed and is ready for immediate execution given the remainder of its expected arguments.",
+
+  closure: "A function that 'captures' values that were nearby where it was born.",
+
+  "first-class": "Something that is just a value.  A first-class function is one that can go anywhere that any other value can go.",
+
+  scope: "The variable value resolution scheme determining the context where a variable contains some given value."
 };
